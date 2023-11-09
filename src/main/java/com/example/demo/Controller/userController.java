c56f82e5b4a3d6dfa0c2393938fe8e05bfc357d8
sakura-03-02 <149939345+sakura-03-02@users.noreply.github.com>
2023-11-09 17:45:16 +0800

    Update userController.java

--- src/main/java/com/example/demo/Controller/userController.java
+++ src/main/java/com/example/demo/Controller/userController.java
@@ -21,7 +21,7 @@ public class userController
     public List<User> findAll()
     {
         List<User> users = userService.findAll();
-        System.out.println("huchao444");
+        System.out.println("huchao555");
         return users;
     }
     @PostMapping
